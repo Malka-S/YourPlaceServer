035afa013eb78d8bbd614d4e9f73ee8a944ada77
malkas <malkasvei@gmail.com>
2021-02-15 11:59:16 +0200

    Revert "Attachment file to mail- not complete"
    This reverts commit 7af4907fd2665a03c8a995a28fd9d0b0334a4a85.

--- BLL/EmailService.cs
+++ BLL/EmailService.cs
@@ -1,50 +1,14 @@
-using DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace BLL
 {
   public class EmailService
   {
-
-
-    public static void mailwithpic(int event_id)
-    {
-      try
-      {
-        MailMessage mail = new MailMessage();
-        SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-        mail.From = new MailAddress("malkaspaintings@gmail.com");
-        List<string> guestMail =GuestDal.GetGuestMail(event_id);
-        var httpRequest = HttpContext.Current.Request;
-
-        foreach (var item in guestMail)
-        {
-          mail.To.Add(item);
-          mail.Body = " \n  :מייל תזכורת לארוע שלכם    ";
-          Attachment attachment;
-          attachment = new Attachment(EventDal.GetInvatation(event_id));
-          var filePath = HttpContext.Current.Server.MapPath("~/UploadFile/" + attachment);
-
-          mail.Attachments.Add(attachment);
-          SmtpServer.Port = 587;
-          SmtpServer.Credentials = new System.Net.NetworkCredential("malkaspaintings@gmail.com", "Malkaart332");
-          SmtpServer.EnableSsl = true;
-
-          SmtpServer.Send(mail);
-        }
-      }
-      catch (Exception e)
-      {
-        throw e;
-      }
-    }
-
 
     public static void mail(string to, string body)
     {
@@ -55,8 +19,7 @@ namespace BLL
         mail.From = new MailAddress("malkaspaintings@gmail.com");
         mail.To.Add(to);
 
-        mail.Body = " \n  :מייל תזכורת לארוע שלכם    " + body;
-
+        mail.Body = " \n  :מייל תזכורת לארוע שלכם    " +body;
         SmtpServer.Port = 587;
         SmtpServer.Credentials = new System.Net.NetworkCredential("malkaspaintings@gmail.com", "Malkaart332");
         SmtpServer.EnableSsl = true;
@@ -68,6 +31,6 @@ namespace BLL
         throw e;
       }
     }
+
   }
 }
-
